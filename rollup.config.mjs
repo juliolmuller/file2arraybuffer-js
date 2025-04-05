dc90a571b4ab80bbe7966e6071df5c9cd5497fc2
juliolmuller <juliolmuller@outlook.com>
2025-04-05 02:37:19 -0300

    refactor: lint source code

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,24 +1,24 @@
 /* eslint-env node */
-import babelPlugin from '@rollup/plugin-babel'
-import commonJsPlugin from '@rollup/plugin-commonjs'
-import nodeResolvePlugin from '@rollup/plugin-node-resolve'
-import terserPlugin from '@rollup/plugin-terser'
-import typeScriptPlugin from '@rollup/plugin-typescript'
-import { defineConfig } from 'rollup'
-import deletePlugin from 'rollup-plugin-delete'
-import declarationsPlugin from 'rollup-plugin-dts'
-import esBuildPlugin from 'rollup-plugin-esbuild'
+import babelPlugin from '@rollup/plugin-babel';
+import commonJsPlugin from '@rollup/plugin-commonjs';
+import nodeResolvePlugin from '@rollup/plugin-node-resolve';
+import terserPlugin from '@rollup/plugin-terser';
+import typeScriptPlugin from '@rollup/plugin-typescript';
+import { defineConfig } from 'rollup';
+import deletePlugin from 'rollup-plugin-delete';
+import declarationsPlugin from 'rollup-plugin-dts';
+import esBuildPlugin from 'rollup-plugin-esbuild';
 
-import packageMeta from './package.json' with { type: 'json' }
+import packageMeta from './package.json' with { type: 'json' };
 
-const inputFileName = 'src/index.ts'
+const inputFileName = 'src/index.ts';
 const bundleBanner = `/**
  * file2arraybuffer v${packageMeta.version}
  *
  * @author ${packageMeta.author}.
  * @license ${packageMeta.license} - 2020-${new Date().getFullYear()}
  */
-`
+`;
 
 export default defineConfig([
   // UMD for legacy browsers
@@ -38,17 +38,12 @@ export default defineConfig([
         format: 'umd',
         sourcemap: 'inline',
         banner: bundleBanner,
-        plugins: [
-          terserPlugin(),
-        ],
+        plugins: [terserPlugin()],
       },
     ],
     plugins: [
       deletePlugin({
-        targets: [
-          'build/*',
-          'dist/*',
-        ],
+        targets: ['build/*', 'dist/*'],
       }),
       nodeResolvePlugin(),
       commonJsPlugin(),
@@ -99,8 +94,6 @@ export default defineConfig([
       file: 'build/index.d.ts',
       format: 'es',
     },
-    plugins: [
-      declarationsPlugin(),
-    ],
+    plugins: [declarationsPlugin()],
   },
-])
+]);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { default } from './file-to-arraybuffer'
+export { default } from './file-to-arraybuffer';
